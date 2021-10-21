3b00aadf7dd436e98f9fe05f94e8733f44e4e503
Opilane TTHK <opilane@tthk.ee>
2021-10-21 09:53:23 +0300

    xd

---
+++ EpicFight/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero, villain;
+            hero = RandomHero();
+            heroWeapon = RandomWeapon();
+            villain = RandomVillain();
+            villainWeapon = RandomWeapon();
+            
+            Console.WriteLine($"{hero} will fight {villain}.");
+            Console.WriteLine ($"{hero} will fight {heroweapon}")
+        }
+    
+
+        private static int RandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+
+
+        private static string RandomHero()
+        {
+            string[] heroes = { "Batman", "Spongebob", "Spiderman", "Patric", "Laura Croft" };
+            
+            int randomIndex = rnd.Next(0, heroes.Length);
+
+            string randomHero = heroes[randomIndex];
+            return randomHero;
+        }
+    
+        private static string RandomVillain()
+        {
+            string[] villains = { "Joker", "Darth Vader", "Thanos", "Loki", "T-1000" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, villains.Length);
+
+            string randomVillain = villains[randomIndex];
+            return randomVillain;
+        }
+    
+      private static string RandomWeapon()
+        {
+
+        }
+    
+    }
+}
---
+++ FunctionsWithReturns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FunctionsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] food = { "chicken wings", "burger", "french fries", "salad", "chicken nuggets" };
+            string[] drinks = { "coca-cola", "fanta", "sprite", "still water" };
+            string[] movie = { "Scream", "The Purge", "Bird Box", "Dune" };
+
+            //DisplayRandomFromArray(foods);
+            //DisplayRandomFromArray(drinks);
+            DisplayRandomFromArray(food);
+            DisplayRandomFromArray(drinks);
+            DisplayRandomFromArray(movie);
+
+            string randomSnack = PickRandomFromArray(food);
+            string randomDrink = PickRandomFromArray(drinks);
+            string randomMovie = PickRandomFromArray(movie);
+            Console.WriteLine($"Computer picked {randomSnack} and {randomDrink} for a {randomMovie} night. ");
+
+        }
+        
+        private static string PickRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomElement = someArray[randomIndex];
+
+            return randomElement;
+        }
+       private static void DisplayRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            Console.WriteLine($"computer has chosen {someArray[randomIndex]}");
+        }
+    
+    
+    
+    }
+}
